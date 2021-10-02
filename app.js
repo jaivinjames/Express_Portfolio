0d2fe25ced094e5ed4136210d1e66441bd700b63
jaivinjames <jaivinjames7@gmail.com>
2021-10-02 18:13:22 +0530

    Updated using partials

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -36,7 +36,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { title : 'Error'});
 });
 
 module.exports = app;
